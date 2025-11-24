2abcfeb085b185ec5c9808ad1d08dd554ee5a6bb
himchan00 <himchan00@gmail.com>
2025-11-24 12:59:29 +0900

    Add detailed balance experiment scripts

---
+++ energy_sampling/scripts/many_well_db.sh
@@ -0,0 +1,9 @@
+for seed in 0 1 2
+do
+    CUDA_VISIBLE_DEVICES=0, python train.py \
+        --seed $seed \
+        --t_scale 1. --energy many_well --pis_architectures --zero_init --clipping\
+        --mode_fwd db --lr_policy 1e-3 --lr_flow 1e-2 \
+        --exploratory --exploration_wd --exploration_factor 0.2 \
+
+done
