0dd8f8ec59ee738d5219e23c450d655f2418192e
himchan00 <himchan00@gmail.com>
2025-11-25 13:09:35 +0900

    Save final db scripts

--- energy_sampling/scripts/25gmm_db.sh
+++ energy_sampling/scripts/25gmm_db.sh
@@ -4,6 +4,7 @@ do
         --seed $seed \
         --t_scale 5. --energy 25gmm --pis_architectures --zero_init --clipping\
         --mode_fwd db --lr_policy 1e-3 --lr_flow 1e-2 \
+        --conditional_flow_model \
         --exploratory --exploration_wd --exploration_factor 0.2 \
 
 done
--- energy_sampling/scripts/many_well_db.sh
+++ energy_sampling/scripts/many_well_db.sh
@@ -4,6 +4,7 @@ do
         --seed $seed \
         --t_scale 1. --energy many_well --pis_architectures --zero_init --clipping\
         --mode_fwd db --lr_policy 1e-3 --lr_flow 1e-2 \
+        --conditional_flow_model \
         --exploratory --exploration_wd --exploration_factor 0.2 \
 
 done
