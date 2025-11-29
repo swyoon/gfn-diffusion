53c614c1ecb3a6737ba1c2948dd180c667788aa9
hjgit1 <hyeokju0502@gmail.com>
2025-11-29 15:34:04 +0900

    Add lj55 script

---
+++ energy_sampling/scripts/lj55_db.sh
@@ -0,0 +1,10 @@
+for seed in 0
+do
+    CUDA_VISIBLE_DEVICES=1, python train.py \
+        --seed $seed \
+        --t_scale 1. --energy lj55 --pis_architectures --zero_init --clipping\
+        --mode_fwd db --lr_policy 1e-4 --lr_flow 1e-3 \
+        --conditional_flow_model \
+        --exploratory --exploration_wd --exploration_factor 0.2 \
+        --hidden_dim 128 --t_emb_dim 128 --batch_size 128 --epochs 50000
+done
---
+++ energy_sampling/scripts/lj55_sub_tb_exp_lp.sh
@@ -0,0 +1,11 @@
+for seed in 0
+do
+    CUDA_VISIBLE_DEVICES=0, python train.py \
+        --seed $seed \
+        --t_scale 1. --energy lj55 --pis_architectures --zero_init --clipping\
+        --mode_fwd subtb --lr_policy 1e-4 --lr_flow 1e-3 \
+        --partial_energy --conditional_flow_model\
+        --langevin --epochs 50000 \
+        --exploratory --exploration_wd --exploration_factor 0.2 \
+        --hidden_dim 128 --t_emb_dim 128 --batch_size 128
+done
